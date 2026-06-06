e8dca9f51653bfd8e848c7c5c4aab7e5a8bddfea
ahzagamerluahub-glitch <ahza.gamer.lua.hub@gmail.com>
2026-06-06 15:04:39 +0700

    Add configuration file with store details and pricing

---
+++ config.js
@@ -0,0 +1,12 @@
+const CONFIG = {
+  STORE_NAME: "Percetakan Serasi",
+  ADMIN_WHATSAPP: "6285784597141",
+  ADMIN_EMAIL: "evosrezasyah@gmail.com",
+
+  PRICING: {
+    yasin: {
+      under100: 8000,
+      over100: 7500
+    }
+  }
+};
