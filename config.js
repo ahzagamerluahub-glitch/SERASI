d7583a8d4aeb49a71bde0528813279a29ff3b9e0
ahzagamerluahub-glitch <ahza.gamer.lua.hub@gmail.com>
2026-06-06 15:06:09 +0700

    Remove unused configuration fields from config.js

--- config.js
+++ config.js
@@ -1,12 +1,9 @@
 const CONFIG = {
-  STORE_NAME: "Percetakan Serasi",
   ADMIN_WHATSAPP: "6285784597141",
-  ADMIN_EMAIL: "evosrezasyah@gmail.com",
-
-  PRICING: {
-    yasin: {
-      under100: 8000,
-      over100: 7500
-    }
-  }
+  STORE_NAME: "Percetakan Serasi"
 };
+
+// data sementara desain yang dipilih
+let selectedImage = "";
+let userLat = null;
+let userLng = null;
