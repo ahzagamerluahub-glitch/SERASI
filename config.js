134b031005de23b37aaad6f8fca0175339e965b6
ADMIN-SERASI <ahza.gamer.lua.hub@gmail.com>
2026-06-07 07:52:57 +0700

    Refactor config.js to simplify variable declarations

--- config.js
+++ config.js
@@ -1,9 +1,5 @@
-const CONFIG = {
-  ADMIN_WHATSAPP: "6285784597141",
-  STORE_NAME: "Percetakan Serasi"
-};
+const ADMIN_WA = "6285784597141";
 
-// data sementara desain yang dipilih
-let selectedImage = "";
-let userLat = null;
-let userLng = null;
+let selectedImg = "";
+let lat = "";
+let lng = "";
